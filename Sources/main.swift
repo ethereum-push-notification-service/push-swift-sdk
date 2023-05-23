14d3a0c5e046bfe77d1ae74f50d2b412470430dc
Abishek Bashyal <abishek_bashyal@outlook.com>
2023-05-23 15:40:49 +0545

    aes enc added

---
+++ Sources/main.swift
@@ -0,0 +1,36 @@
+import CryptoKit
+import Foundation
+
+func aesGCMDecrypt(ciphertext: Data,tag:Data, key: SymmetricKey, nonce: Data) throws -> Data {
+  let sealedBox = try AES.GCM.SealedBox.init(
+    nonce: AES.GCM.Nonce(data: nonce), ciphertext: ciphertext, tag: tag)
+  let decryptedData = try AES.GCM.open(sealedBox, using: key)
+  return decryptedData
+}
+
+func aesGCMEncrypt(plaintext: Data, key: SymmetricKey, nonce: Data) throws -> (Data, Data) {
+  let sealedBox = try AES.GCM.seal(
+    plaintext, using: key, nonce: AES.GCM.Nonce(data: nonce))
+  return (sealedBox.ciphertext, sealedBox.tag)
+}
+
+func deriveKey(key: String, salt: String) -> SymmetricKey {
+  let key: SymmetricKey = SymmetricKey(data: key.data(using: .utf8)!)
+  let salt = salt.data(using: .utf8)!
+  let derivedKey = HKDF<SHA256>.deriveKey(inputKeyMaterial: key, salt: salt, outputByteCount: 256)
+  return derivedKey
+}
+
+let ck = "my-256-bit-secret-my-secret-my-s"
+let _nonce = "dfc04cc878288".data(using: .utf8)!
+let ckkey = SymmetricKey(data: ck.data(using: .utf8)!)
+
+let chyper = "This is a good place to find a city"
+let (ciphertext, tag) = try aesGCMEncrypt(
+  plaintext: chyper.data(using: .utf8)!, key: ckkey, nonce: _nonce)
+
+print("Enc data ->", ciphertext.base64EncodedString())
+
+
+let res = try aesGCMDecrypt(ciphertext: ciphertext,tag:tag, key: ckkey, nonce: _nonce)
+print("Dec data ->", String(data: res, encoding: .utf8)!)
